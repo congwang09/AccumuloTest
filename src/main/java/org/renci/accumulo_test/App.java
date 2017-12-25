47d7b17d5c6a6e7415bbe1a37bb5017cc24c12fb
Cong Wang <cwang@renci.org>
2017-12-25 03:10:21 +0000

    updates

--- src/main/java/org/renci/accumulo_test/App.java
+++ src/main/java/org/renci/accumulo_test/App.java
@@ -1,10 +1,20 @@
-package org.renci.accumulo_test;
+package org.renci;
 
 /**
  * Hello world!
  *
- */
+
+	public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+**/
+
 import org.apache.accumulo.core.client.AccumuloException;
+
 import org.apache.accumulo.core.client.AccumuloSecurityException;
 import org.apache.accumulo.core.client.BatchWriter;
 import org.apache.accumulo.core.client.Connector;
@@ -16,20 +26,26 @@ import org.apache.accumulo.core.client.admin.TableOperations;
 import org.apache.accumulo.core.data.Mutation;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.security.ColumnVisibility;
-import org.apache.hadoop.io.Text;
+//import org.apache.hadoop.io.Text;
 
 public class App {
-
- public static void main(String[] args) throws AccumuloException, AccumuloSecurityException,TableNotFoundException, TableExistsException {
+  public static void main (String[] args) throws AccumuloException, AccumuloSecurityException{
+  //public static void main(String[] args) throws AccumuloException, AccumuloSecurityException,TableNotFoundException, TableExistsException {
         // Constants
-        String instanceName = "default";
-        String zooServers = "128.227.10.9, 128.227.10.7, 128.227.10.10"; // Provide list of zookeeper server here. For example, localhost:2181 
+        String instanceName = "exogeni";
+        String zooServers = "172.16.100.5"; // Provide list of zookeeper server here. For example, localhost:2181 
         String userName = "root"; // Provide username
         String password = "secret"; // Provide password
         // Connect
+        //System.out.println("hi");
+
+
         Instance inst = new ZooKeeperInstance(instanceName,zooServers);
+	System.out.println("checkpoint1");
         Connector conn = inst.getConnector(userName, password);
+	System.out.println("checkpoint2");
        // Create our new table
+/*
         String tableName = "testTable";
         TableOperations ops = conn.tableOperations();
          if (ops.exists(tableName)) {
@@ -51,5 +67,6 @@ public class App {
         bw.addMutation(mutation);
         // close the batch writer
         bw.close();
+*/
     }
 }
