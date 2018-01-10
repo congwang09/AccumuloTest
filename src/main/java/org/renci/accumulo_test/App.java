2bf7b8153cba6c8ca7e1af35b93d570e78511bea
CW <cwang@renci.org>
2018-01-10 19:58:48 +0000

    working

--- src/main/java/org/renci/accumulo_test/App.java
+++ src/main/java/org/renci/accumulo_test/App.java
@@ -26,14 +26,15 @@ import org.apache.accumulo.core.client.admin.TableOperations;
 import org.apache.accumulo.core.data.Mutation;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.security.ColumnVisibility;
-//import org.apache.hadoop.io.Text;
+import org.apache.log4j.Logger;
+import org.apache.hadoop.io.Text;
 
 public class App {
-  public static void main (String[] args) throws AccumuloException, AccumuloSecurityException{
+  public static void main (String[] args) throws AccumuloException, AccumuloSecurityException,TableNotFoundException, TableExistsException{
   //public static void main(String[] args) throws AccumuloException, AccumuloSecurityException,TableNotFoundException, TableExistsException {
         // Constants
         String instanceName = "exogeni";
-        String zooServers = "172.16.100.5"; // Provide list of zookeeper server here. For example, localhost:2181 
+        String zooServers = "172.16.100.5,172.16.100.4,172.16.100.1"; // Provide list of zookeeper server here. For example, localhost:2181 
         String userName = "root"; // Provide username
         String password = "secret"; // Provide password
         // Connect
@@ -52,8 +53,15 @@ public class App {
             ops.delete(tableName);
         }
         ops.create(tableName);
+*/
         // Use batch writer to write demo data
-        BatchWriter bw = conn.createBatchWriter(tableName,1000000, 60000, 2);
+        String tableName = "trace";
+        try{
+        	BatchWriter bw = conn.createBatchWriter(tableName,1000000, 60000, 2);
+        } catch (TableNotFoundException e) {
+        	System.out.println("Accumulo table " + tableName + " doesn't exist and cannot be created.");
+		} 
+        
         // set values
         Text rowID = new Text("row1");
         Text colFam = new Text("colFam");
@@ -62,11 +70,13 @@ public class App {
         Value value = new Value("some-value".getBytes());
         // create new mutation and add rowID, colFam, colQual, and value
         Mutation mutation = new Mutation(rowID);
+        
         mutation.put(colFam, colQual, value);
         // add the mutation to the batch writer
         bw.addMutation(mutation);
         // close the batch writer
         bw.close();
-*/
+        
+		System.out.println("checkpoint3");
     }
 }
