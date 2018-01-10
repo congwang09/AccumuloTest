21c663837cfd7f4f6b5f32f9a90fa3b972cb4df0
CW <cwang@renci.org>
2018-01-10 20:58:27 +0000

    accu api tests

--- src/main/java/org/renci/accumulo_test/App.java
+++ src/main/java/org/renci/accumulo_test/App.java
@@ -3,8 +3,7 @@ package org.renci;
 /**
  * Hello world!
  *
-
-	public class App 
+	public class App
 {
     public static void main( String[] args )
     {
@@ -31,10 +30,9 @@ import org.apache.hadoop.io.Text;
 
 public class App {
   public static void main (String[] args) throws AccumuloException, AccumuloSecurityException,TableNotFoundException, TableExistsException{
-  //public static void main(String[] args) throws AccumuloException, AccumuloSecurityException,TableNotFoundException, TableExistsException {
         // Constants
         String instanceName = "exogeni";
-        String zooServers = "172.16.100.5,172.16.100.4,172.16.100.1"; // Provide list of zookeeper server here. For example, localhost:2181 
+        String zooServers = "172.16.100.5,172.16.100.4,172.16.100.1"; // Provide list of zookeeper server here. For example, localhost:2181
         String userName = "root"; // Provide username
         String password = "secret"; // Provide password
         // Connect
@@ -42,26 +40,22 @@ public class App {
 
 
         Instance inst = new ZooKeeperInstance(instanceName,zooServers);
-	System.out.println("checkpoint1");
+	       System.out.println("checkpoint1");
         Connector conn = inst.getConnector(userName, password);
-	System.out.println("checkpoint2");
+	       System.out.println("checkpoint2");
        // Create our new table
-/*
+
         String tableName = "testTable";
         TableOperations ops = conn.tableOperations();
          if (ops.exists(tableName)) {
             ops.delete(tableName);
         }
         ops.create(tableName);
-*/
+
         // Use batch writer to write demo data
-        String tableName = "trace";
-        try{
-        	BatchWriter bw = conn.createBatchWriter(tableName,1000000, 60000, 2);
-        } catch (TableNotFoundException e) {
-        	System.out.println("Accumulo table " + tableName + " doesn't exist and cannot be created.");
-		} 
-        
+        //String tableName = "trace";
+        BatchWriter bw = conn.createBatchWriter(tableName,1000000, 60000, 2);
+
         // set values
         Text rowID = new Text("row1");
         Text colFam = new Text("colFam");
@@ -70,13 +64,13 @@ public class App {
         Value value = new Value("some-value".getBytes());
         // create new mutation and add rowID, colFam, colQual, and value
         Mutation mutation = new Mutation(rowID);
-        
+
         mutation.put(colFam, colQual, value);
         // add the mutation to the batch writer
         bw.addMutation(mutation);
         // close the batch writer
         bw.close();
-        
-		System.out.println("checkpoint3");
+
+        System.out.println("checkpoint3");
     }
 }
