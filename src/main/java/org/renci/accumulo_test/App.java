3ba204bc4147e75018bc009c4ceef78a683f9e9b
Cong Wang <congwang@dhcp-v880-10-00537.1Xwireless.unc.edu>
2018-05-11 15:03:26 -0400

    test

--- src/main/java/org/renci/accumulo_test/App.java
+++ src/main/java/org/renci/accumulo_test/App.java
@@ -28,12 +28,14 @@ import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.security.Authorizations;
 import org.apache.accumulo.core.security.ColumnVisibility;
 import org.apache.log4j.Logger;
+import org.codehaus.jettison.json.JSONObject;
 import org.apache.hadoop.io.Text;
 
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.Value;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -55,7 +57,7 @@ public class App {
 	       System.out.println("checkpoint2");
        // Create our new table
 
-        String tableName = "testTable";
+        String tableName = "trace";
         TableOperations ops = conn.tableOperations();
          if (ops.exists(tableName)) {
             ops.delete(tableName);
@@ -67,7 +69,7 @@ public class App {
         BatchWriter bw = conn.createBatchWriter(tableName,1000000, 60000, 2);
 
         // set values
-        Text rowID = new Text("row1");
+        Text rowID = new Text("id0001");
         Text colFam = new Text("colFam");
         Text colQual = new Text("colQual");
         // set value
@@ -83,8 +85,15 @@ public class App {
 
         System.out.println("checkpoint3");
 
+        
+        /*	    Map<String, Value> out = new HashMap<>();
+	    out = readOneRow( conn,  "trace", new Text("id0001"), new Text("hero"), new Text("alias2"), "secretId");
+	    for (Map.Entry<String, Value> entry : out.entrySet()) {
+	    		System.out.println(entry.getValue());
+	    }*/
+        
         Scanner scan = conn.createScanner(tableName, new Authorizations());
-        scan.setRange(new Range("row1", "row1"));
+        scan.setRange(new Range("id0001", "id0001"));
         Iterator<Map.Entry<Key,Value>> iterator = scan.iterator();
         while (iterator.hasNext()) {
         		Map.Entry<Key, Value> entry = iterator.next();
@@ -93,4 +102,29 @@ public class App {
         		System.out.println(key + " ==> " + gotValue);
         }
     }
+  
+  	public static Map<String, Value> readOneRow(Connector conn, String tableName, Text rowID, Text colFam, Text colQual, String visibility) throws TableNotFoundException, AccumuloException, AccumuloSecurityException {
+		JSONObject output = new JSONObject();
+		//ScannerOpts scanOpts = new ScannerOpts();
+		// Create a scanner
+		Authorizations auths = new Authorizations(visibility);
+		conn.securityOperations().changeUserAuthorizations("root", auths);
+	    Scanner scanner = conn.createScanner(tableName, auths);
+	    //scanner.setBatchSize(scanOpts.scanBatchSize);
+	    // Say start key is the one with key of row
+	    // and end key is the one that immediately follows the row
+	    scanner.setRange(new Range(rowID));
+	    scanner.fetchColumn(colFam, colQual);
+	    Map<String, Value> out = new HashMap<String, Value>();
+	    for (Map.Entry<Key, Value> entry : scanner) {
+	    		Key key = entry.getKey();
+			Value value = entry.getValue();
+	        
+			out.put(key.toString(), value);
+			
+	    		System.out.printf("Key : %-50s  Value : %s\n", entry.getKey(), entry.getValue());
+	    }
+	    scanner.close();
+	    return out;
+	}
 }
